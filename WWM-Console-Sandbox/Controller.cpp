dc7f74b20cb0b7cb7cc9a8b45f6957bc154a720f
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-04 20:07:04 +0200

    Added 2dArray of Question and Answer Strings

--- WWM-Console-Sandbox/Controller.cpp
+++ WWM-Console-Sandbox/Controller.cpp
@@ -4,19 +4,59 @@
 
 void CheckCorrectAnswer()
 {
-	if (SelectedAnswer == CorrectAnswer)
+	if (SelectedAnswer == CurrentCorrectAnswer)
 	{
 		QuestionGrabber();
+	}
+	else
+	{
+		EndGame();
 	}
 		
 }
 void QuestionGrabber()
 {
-
+	SelectedAnswer = "none";
+	QuestionCounter++;
+	
+	switch (QuestionCounter)
+	{
+	case(1): 
+		break;
+	case(2):
+		break;
+	case(3):
+		break;
+	case(4):
+		break;
+	case(5):
+		break;
+	case(6):
+		break;
+	case(7):
+		break;
+	case(8):
+		break;
+	case(9):
+		break;
+	case(10):
+		break;
+	case(11):
+		break;
+	case(12):
+		break;
+	case(13):
+		break;
+	case(14):
+		break;
+	case(15):
+		break;
+	default: cout << "Sorry - no more questions possible!" << endl;
+	}
 }
 void SubmitAnswer()
 {
-
+	CheckCorrectAnswer();
 }
 void ChangeMind()
 {
--- WWM-Console-Sandbox/Controller.h
+++ WWM-Console-Sandbox/Controller.h
@@ -6,30 +6,33 @@ using namespace std;
 
 // -- VARIABLES ---
 
-unsigned short QuestionCounter;
-string CorrectAnswer;
+unsigned short QuestionCounter = 0;
+string CurrentCorrectAnswer;
 string SelectedAnswer;
 
-// Hardcoded Question Data: {Level (easy till 1000 Gold || medium till 16000 Gold || hard till 1 million gold), Question, Four possible answers to display, correct answer}
+// Hardcoded Question Data: {Level, Question, Four possible answers to display, correct answer}
 
-string Question1[7] = { "Easy", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								//50
-string Question2[7] = { "Easy", "What animal is on the Ferrari logo?", "Berlin", "Cairo", "Athens", "Madrid", "Horse" };						// 100
-string Question3[7] = { "Easy", "What is the longest river in South America?", "Amazonas", "Nile", "Volga", "Danube", "Amazonas" };				// 200
-string Question4[7] = { "Easy", "What is the name of Google's browser?", "Berlin", "Cairo", "Athens", "Chrome", "Chrome" };						// 500
-string Question5[7] = { "Easy", "What is the annual academy award usually called?", "Peter", "Lucas", "Oscar", "Jeremy", "Oscar" };				// 1.000*
-string Question6[7] = { "Medium", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };							// 2.000
-string Question7[7] = { "Medium", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };							// 4.000
-string Question8[7] = { "Medium", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };							// 8.000
-string Question9[7] = { "Medium", "How many stars does the EU flag have?", "8", "10", "12", "16", "12" };										// 16.000*
-string Question10[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 32.000
-string Question11[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 64.000
-string Question12[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 125.000
-string Question13[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 250.000
-string Question14[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 500.000
-string Question15[7] = { "Hard", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" };								// 1.000.000
+string Question[15][7] = { 
+	{ "1", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "2", "What animal is on the Ferrari logo?", "Berlin", "Cairo", "Athens", "Madrid", "Horse" },
+	{ "3", "What is the longest river in South America?", "Amazonas", "Nile", "Volga", "Danube", "Amazonas" },
+	{ "4", "What is the name of Google's browser?", "Berlin", "Cairo", "Athens", "Chrome", "Chrome" },
+	{ "5", "What is the annual academy award usually called?", "Peter", "Lucas", "Oscar", "Jeremy", "Oscar" },
+	{ "6", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "7", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "8", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "9", "How many stars does the EU flag have?", "8", "10", "12", "16", "12" },
+	{ "10", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "11", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "12", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "13", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "14", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" },
+	{ "15", "What is the capital of Egypt?", "Berlin", "Cairo", "Athens", "Madrid", "Cairo" } };
 
 // -- FUNCTIONS --
 void CheckCorrectAnswer();
 void QuestionGrabber();
 void SubmitAnswer();
 void ChangeMind();
+void TakeMoney();
+void EndGame();
